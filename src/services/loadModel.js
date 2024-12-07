723031e796dfcdf3d65710d6aadf9d901c5a4011
rifal12330 <rifalmuhamad12330@gmail.com>
2024-12-07 12:35:50 +0700

    first commit

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,14 +1,20 @@
 const { Storage } = require('@google-cloud/storage');
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();  // Pastikan Anda memuat .env file
 
 // Inisialisasi Google Cloud Storage
 const storage = new Storage();
 
 // Ambil bucket dan path file model dari environment variable MODEL_PATH
-const modelPath = 'https://storage.googleapis.com/mlgc-bucket-1933/submissions-model/model.json'; // Format: gs://<bucket_name>/<model_path>
+const modelPath = process.env.MODEL_PATH; // Format: gs://<bucket_name>/<model_path>
 if (!modelPath) {
     throw new Error('MODEL_PATH environment variable is not set');
+}
+
+// Periksa apakah MODEL_PATH sesuai dengan format 'gs://<bucket_name>/<path>'
+if (!modelPath.startsWith('gs://')) {
+    throw new Error('MODEL_PATH harus dimulai dengan gs://');
 }
 
 const bucketName = modelPath.split('/')[2];
